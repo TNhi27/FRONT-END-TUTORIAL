22c7bc24bf898c933865540991e43b57d2632365
TNhi27 <truongta.270201@gmail.com>
2021-08-31 14:58:55 +0700

    d2

---
+++ day2/index.js
@@ -0,0 +1,9 @@
+var btn = document.getElementById("btn");
+var light = document.getElementById("light")
+
+function abc() {
+
+    light.classList.toggle("on")
+    btn.classList.toggle("active")
+
+}
